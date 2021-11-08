0dc9ca72fd326be43d3e617aaa4cbf80f2e3a566
Alejandro Salazar Mejía <alejosalazarmejia.2000@gmail.com>
2021-11-08 11:04:56 -0500

    Código depurado

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -9,11 +9,6 @@ public class Asignatura {
 
     Asignatura() {
         this("Sin nombre");
-        this(0);
-    }
-
-    Asignatura(int codigoInterno) {
-        this("Sin nombre", codigoInterno, 0);
     }
 
     Asignatura(int codigoExterno) {
@@ -34,10 +29,6 @@ public class Asignatura {
         this.codigoInterno = codigoInterno;
         this.codigoExterno = codigoExterno;
         this.nombre = nombre;
-    }
-
-    void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
     }
 
     void cambiarDatos(int codigoExterno) {
